90de5ddea215b76fbcfb35be0bcd344ecba5a737
PRATYUSH PRATEEK <69977797+maverick-op@users.noreply.github.com>
2022-06-16 13:17:03 +0530

    Create 540. Single Element in a Sorted Array.cpp

---
+++ Binary search/540. Single Element in a Sorted Array.cpp
@@ -0,0 +1,51 @@
+// You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+
+// Return the single element that appears only once.
+
+// Your solution must run in O(log n) time and O(1) space.
+
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& a) {
+        
+        if(a.size() == 1)
+        {
+            return a[0];
+        }
+        
+        int start = 0;
+        int end = a.size()-1;
+        
+        int mid = 0;
+        int ans = 0;
+        
+        while(start < end)
+        {
+            
+            mid = start + (end-start)/2;
+            
+            if(mid%2==0)
+            {
+                ans = mid+1;
+            }
+            else
+            {
+                ans = mid-1;
+            }
+            
+            if(a[mid] == a[ans])
+            {
+                start = mid+1;
+            }
+            else
+            {
+                end = mid;
+            }
+            
+        }
+        
+        return a[start];
+        
+    }
+};
