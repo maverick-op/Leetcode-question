6dfa3f2eb62560e52764d9eda5c0f1ca524dad06
PRATYUSH PRATEEK <69977797+maverick-op@users.noreply.github.com>
2022-06-22 19:17:21 +0530

    Create 1026. Maximum Difference Between Node and Ancestor.cpp

---
+++ Binary tree/1026. Maximum Difference Between Node and Ancestor.cpp
@@ -0,0 +1,24 @@
+Aprroach:- Traverse through left part and right part and compare in which part
+the differnce between the nodes is maximum and return that part
+
+
+
+class Solution {
+public:
+    int maxAncestorDiff(TreeNode* root, int maxx=0, int minn=1e7) {
+        
+        int ans = 0;
+        
+        if(root == NULL)
+        {
+            return (maxx-minn);
+        }
+        
+        maxx = max(root->val, maxx);
+        minn = min(root->val, minn);
+        
+        ans = max(maxAncestorDiff(root->left,maxx,minn),maxAncestorDiff(root->right,maxx,minn));
+        
+        return ans;
+    }
+};
