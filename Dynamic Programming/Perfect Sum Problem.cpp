a4c2ee96cf3f82ba439ada9fad062921f0071ef2
PRATYUSH PRATEEK <69977797+maverick-op@users.noreply.github.com>
2022-06-22 15:11:40 +0530

    Create Perfect Sum Problem.cpp

---
+++ Dynamic Programming/Perfect Sum Problem.cpp
@@ -0,0 +1,77 @@
+https://practice.geeksforgeeks.org/problems/perfect-sum-problem5633/1/#
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+
+	public:
+	int perfectSum(int a[], int n, int sum)
+	{
+        // Your code goes here
+        int dp[n+1][sum+1];
+        int mod=1e9+7;
+        
+        for(int i=0; i<n+1; i++)
+        {
+            for(int j=0; j<sum+1; j++)
+            {
+                if(i==0)
+                {
+                    dp[i][j] = 0;
+                }
+                if(j==0)
+                {
+                    dp[i][j] = 1;
+                }
+            }
+        }
+        
+        for(int i=1; i<n+1; i++)
+        {
+            for(int j=0; j<sum+1; j++)
+            {
+                if(a[i-1] <= j)
+                {
+                    dp[i][j] = (dp[i-1][j-a[i-1]])%mod + (dp[i-1][j]%mod) % mod;
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[n][sum]%mod;
+	}
+	  
+};
+
+// { Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, sum;
+
+        cin >> n >> sum;
+
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.perfectSum(a, n, sum) << "\n";
+	     
+    }
+    return 0;
+}
+  // } Driver Code Ends
