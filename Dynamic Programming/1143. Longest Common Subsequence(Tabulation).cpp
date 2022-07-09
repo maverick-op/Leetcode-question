4461540ca1107abd361e2b0435e2db126a29b6f0
PRATYUSH PRATEEK <69977797+maverick-op@users.noreply.github.com>
2022-07-09 14:44:36 +0530

    Create 1143. Longest Common Subsequence(Tabulation).cpp

---
+++ Dynamic Programming/1143. Longest Common Subsequence(Tabulation).cpp
@@ -0,0 +1,39 @@
+int longestCommonSubsequence(string s1, string s2) {
+        
+        int n= s1.size();
+        int m = s2.size();
+        // memset(dp,-1,sizeof(dp));
+        // int ans = help(s1,s2,n,m);
+        // return ans;
+        
+        int dp[n+1][m+1];                     //Tabulation
+        
+        for(int i=0; i<n+1; i++)
+        {
+            for(int j=0; j<m+1; j++)
+            {
+                if(i==0 || j==0)
+                {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        
+        for(int i=1; i<n+1; i++)
+        {
+            for(int j=1; j<m+1; j++)
+            {
+                if(s1[i-1] == s2[j-1])
+                {
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+};
