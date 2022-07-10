c9bfc4d9818df679ccea286fe1397cb0ac788545
PRATYUSH PRATEEK <69977797+maverick-op@users.noreply.github.com>
2022-07-10 13:07:02 +0530

    Create Longest Common Substring .cpp

---
+++ Dynamic Programming/Longest Common Substring .cpp
@@ -0,0 +1,70 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    
+    int longestCommonSubstr (string x, string y, int n, int m)
+    {
+        // your code here
+       int dp[n+1][m+1];
+       
+       for(int i=0; i<n+1; i++)
+       {
+           for(int j=0; j<m+1; j++)
+           {
+               if(i==0 || j==0)
+               {
+                   dp[i][j] = 0;
+               }
+           }
+       }
+       
+       for(int i=1; i<n+1; i++)
+       {
+           for(int j=1; j<m+1; j++)
+           {
+               if(x[i-1] == y[j-1])
+               {
+                   dp[i][j] = 1+(dp[i-1][j-1]);
+               }
+               else
+               {
+                   dp[i][j] = 0;
+               }
+           }
+       }
+       
+       int ans = INT_MIN;
+       
+       for(int i=0; i<n+1; i++)
+       {
+           for(int j=0; j<m+1; j++)
+           {
+               ans = max(ans,dp[i][j]);
+           }
+       }
+       
+       return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n, m; cin >> n >> m;
+        string s1, s2;
+        cin >> s1 >> s2;
+        Solution ob;
+
+        cout << ob.longestCommonSubstr (s1, s2, n, m) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+  // } Driver Code Ends
