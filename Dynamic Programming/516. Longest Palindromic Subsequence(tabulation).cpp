fcafcebf9be7906e6037c26450b940c23cc7ff27
PRATYUSH PRATEEK <69977797+maverick-op@users.noreply.github.com>
2022-07-15 12:18:40 +0530

    Create 516. Longest Palindromic Subsequence(tabulation).cpp

---
+++ Dynamic Programming/516. Longest Palindromic Subsequence(tabulation).cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int longestPalindromeSubseq(string x) {
+        
+        string y = x;
+        reverse(x.begin(),x.end());
+        int n = x.size();
+        int m = y.size();
+        
+        int dp[n+1][m+1];
+        
+        for(int i=0; i<n+1; i++)
+        {
+            for(int j=0; j<m+1; j++)
+            {
+                if(i==0 || j==0)
+                {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        
+        for(int i=1; i<n+1; i++)
+        {
+            for(int j=1; j<m+1; j++)
+            {
+                if(x[i-1] == y[j-1])
+                {
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+};
