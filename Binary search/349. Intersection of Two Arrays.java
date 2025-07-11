94ea9e8137187304063c5aa73a4e34c308741797
PRATYUSH PRATEEK <69977797+maverick-op@users.noreply.github.com>
2025-07-11 23:25:18 +0530

    Update 349. Intersection of Two Arrays.java

--- Binary search/349. Intersection of Two Arrays.java
+++ Binary search/349. Intersection of Two Arrays.java
@@ -1,1 +1,37 @@
+https://leetcode.com/problems/intersection-of-two-arrays/?envType=problem-list-v2&envId=binary-search
 
+class Solution {
+    public int[] intersection(int[] a, int[] b) {
+        
+        int n = a.length;
+        int m = b.length;
+
+        Set<Integer> s1 = new HashSet<>();
+        Set<Integer> s2 = new HashSet<>();
+
+        for(int i : a)
+        {
+            s1.add(i);
+        }
+        for(int i : b)
+        {
+            s2.add(i);
+        }
+        Set<Integer> ans = new HashSet<>();
+
+        for(Integer i : s1)
+        {
+            if(s2.contains(i))
+            {
+                ans.add(i);
+            }
+        }
+        int[] arr = new int[ans.size()];
+        int j = 0;
+        for (Integer val : ans) {
+            arr[j] = val.intValue();
+            j++;
+        }
+        return arr;
+    }
+}
